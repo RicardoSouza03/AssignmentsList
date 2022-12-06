44aff5dffb803d47e7d1fb5aa5f0c35cc6c51bb1
Ricardo_Souza <rl03souza@gmail.com>
2022-12-06 15:18:37 -0300

    refactor: assignments migration, adding userId foreingKey

--- src/migrations/20221206180416-assignments.js
+++ src/migrations/20221206180416-assignments.js
@@ -5,6 +5,17 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('assignments', {
       id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      userId: { 
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        field: 'user_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
       description: Sequelize.STRING,
       createdAt: Sequelize.DATE,
       updatedAt: Sequelize.DATE
