72bc7e5a1ad66d48bc91e0d97ba2a2aa84818f98
Ricardo_Souza <rl03souza@gmail.com>
2022-12-06 16:11:46 -0300

    fix: removing port key from config file

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,6 @@ const config = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
   dialect: 'mysql',
 }
 
