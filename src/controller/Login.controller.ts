4692c75caba1a5cf63ece38d9cc7b8381d495281
Ricardo_Souza <rl03souza@gmail.com>
2023-02-07 18:24:58 -0300

    Refactor: controller to return best response

--- src/controller/Login.controller.ts
+++ src/controller/Login.controller.ts
@@ -4,7 +4,11 @@ import { LoginService } from '../service';
 export default class LoginController {
   public registerUser = async (req: Request, res: Response): Promise<Response> => {
     const userInfo = req.body;
-    const createdUser = await LoginService.createUser(userInfo);
-    return res.status(200).json(createdUser);
+    const token = await LoginService.createUser(userInfo);
+    if (!token) {
+      return res.status(500).json({ message: 'Ops! Houve um erro interno, tente novamente mais tarde.' });
+    }
+
+    return res.status(201).json({ token: token });
   }
 }
