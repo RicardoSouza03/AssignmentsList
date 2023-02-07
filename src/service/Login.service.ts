b4dbe8f72f996c17f0ff81e428de3d7e41edddd2
Ricardo_Souza <rl03souza@gmail.com>
2023-02-07 20:01:31 -0300

    Refactor: createUser method, applie validations

--- src/service/Login.service.ts
+++ src/service/Login.service.ts
@@ -2,15 +2,20 @@ import IUser from '../interfaces/IUser';
 import User from '../database/models/User';
 import { encryptPassword } from '../utils/bcrypt';
 import { generateToken } from '../utils/jwtToken';
+import LoginValidator from '../utils/validations/Login.validations';
 
 export default class LoginService {
-  public static async createUser(user: Omit<IUser, 'id'>): Promise<string | void> {
+  private static loginValidations = new LoginValidator();
+
+  public static async createUser(user: Omit<IUser, 'id'>): Promise<string> {
+    this.loginValidations.checkUserInfo(user);
+    
     const hash = encryptPassword(user.password);
     const createdUser = await User.create({ ...user, password: hash });
     
-    if (createdUser) {
-      const token = generateToken(user);
-      return token;
-    }
+    this.loginValidations.checkUserCreation(createdUser);
+
+    const token = generateToken(user);
+    return token;
   }
 }
