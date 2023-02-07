1d432b3141701eb1b3dfc29acbcfb04ccabc7246
Ricardo_Souza <rl03souza@gmail.com>
2023-02-07 17:53:09 -0300

    Refactor: config file for db connection

--- src/database/config/config.ts
+++ src/database/config/config.ts
@@ -2,9 +2,10 @@ require('dotenv').config();
 import { Options } from 'sequelize'
 
 const config: Options = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  host: process.env.MYSQL_HOST,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '1234',
+  database: 'assigmentList',
+  host: process.env.DB_HOST || 'localhost',
   dialect: 'mysql',
 }
 
