f0e7fd42c4a0ed47bbac9a2c2b9a707149c75f9c
Ricardo_Souza <rl03souza@gmail.com>
2023-02-07 17:17:11 -0300

    Chore: making method createUser static

--- src/service/Login.service.ts
+++ src/service/Login.service.ts
@@ -3,7 +3,7 @@ import User from '../database/models/User';
 import { encryptPassword } from '../utils/bcrypt';
 
 export default class LoginService {
-  public async createUser(user: Omit<IUser, 'id'>): Promise<IUser> {
+  public static async createUser(user: Omit<IUser, 'id'>): Promise<IUser> {
     const hash = encryptPassword(user.password);
     const createdUser = await User.create({ ...user, password: hash });
     return createdUser;
