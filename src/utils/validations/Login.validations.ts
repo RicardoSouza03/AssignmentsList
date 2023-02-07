e5d7d2ec228a07c83ffce23d2b3b32b4d23c6c2f
Ricardo_Souza <rl03souza@gmail.com>
2023-02-07 19:59:25 -0300

    Feat: creating login validations with custom errors

---
+++ src/utils/validations/Login.validations.ts
@@ -0,0 +1,43 @@
+import IUser from '../../interfaces/IUser';
+import InternalError from '../erros/InternalError';
+import InvalidParam from '../erros/InvalidParam';
+import MissinParamError from '../erros/MissinParam';
+
+export interface ILoginValidator {
+  checkUserInfo: (user: IUser) => void;
+  checkUserEmail: ( email: string) => void;
+  checkUserName: ( name: string) => void;
+  checkUserPassword: ( password: string) => void;
+  checkUserCreation: (user: IUser) => void;
+}
+
+export default class LoginValidator implements ILoginValidator {
+  checkUserEmail (email: string): void {
+    if(!email) throw new MissinParamError('O campo "email" é obrigatório.');
+
+    if(!email.match(/\S+@\S+\.\S+/)) throw new InvalidParam('O "email" deve ser no seguinte formato: "algo@algo.com".');
+
+  };
+
+  checkUserName (name: string): void {
+    if(!name) throw new MissinParamError('O campo "name" é obrigatório.');
+    
+    if(name.length < 2) throw new InvalidParam('O nome deve conter ao menos 2 caracteres.');
+  };
+
+  checkUserPassword (password: string): void {
+    if(!password) throw new MissinParamError('O campo "password" é obrigatório.');
+  
+    if(password.length <= 3) throw new InvalidParam('A senha deve conter mais que três digitos.');
+  };
+  
+  checkUserInfo (user: IUser): void {
+    this.checkUserEmail(user.email);
+    this.checkUserName(user.name);
+    this.checkUserPassword(user.password);
+  };
+
+  checkUserCreation (user: IUser): void {
+    if (!user) throw new InternalError('Ops, ocorreu um erro, por favor tente mais tarde.');
+  }
+}
