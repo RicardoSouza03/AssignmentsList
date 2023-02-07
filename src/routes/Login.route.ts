7457f8e382a41f9e16a1f98a77bc29b342e9085a
Ricardo_Souza <rl03souza@gmail.com>
2023-02-07 17:20:57 -0300

    Feat: creating route for /register on Login

---
+++ src/routes/Login.route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import LoginController from '../controller/Login.controller';
+
+const loginRouter = Router();
+
+const loginController = new LoginController();
+
+loginRouter.post('/register', loginController.registerUser);
