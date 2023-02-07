16c6610908957a1480612c4ad86700adef73dc0e
Ricardo_Souza <rl03souza@gmail.com>
2023-02-07 18:24:31 -0300

    Refactor: createUser method to return token to user

--- src/service/Login.service.ts
+++ src/service/Login.service.ts
@@ -1,11 +1,16 @@
 import IUser from '../interfaces/IUser';
 import User from '../database/models/User';
 import { encryptPassword } from '../utils/bcrypt';
+import { generateToken } from '../utils/jwtToken';
 
 export default class LoginService {
-  public static async createUser(user: Omit<IUser, 'id'>): Promise<IUser> {
+  public static async createUser(user: Omit<IUser, 'id'>): Promise<string | void> {
     const hash = encryptPassword(user.password);
     const createdUser = await User.create({ ...user, password: hash });
-    return createdUser;
+    
+    if (createdUser) {
+      const token = generateToken(user);
+      return token;
+    }
   }
 }
