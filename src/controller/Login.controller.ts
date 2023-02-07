0953d9231e70bbaae61ce57e6bbaff5737771982
Ricardo_Souza <rl03souza@gmail.com>
2023-02-07 20:01:54 -0300

    Refactor: controller, making it cleaner

--- src/controller/Login.controller.ts
+++ src/controller/Login.controller.ts
@@ -5,10 +5,6 @@ export default class LoginController {
   public registerUser = async (req: Request, res: Response): Promise<Response> => {
     const userInfo = req.body;
     const token = await LoginService.createUser(userInfo);
-    if (!token) {
-      return res.status(500).json({ message: 'Ops! Houve um erro interno, tente novamente mais tarde.' });
-    }
-
-    return res.status(201).json({ token: token });
+    return res.status(201).json({ token });
   }
 }
