f733c3e3f093cbb6b17ea92fd5eaa1289936cfeb
Ricardo_Souza <rl03souza@gmail.com>
2023-02-10 15:19:20 -0300

    Feat: creating controller for user login

--- src/controller/Login.controller.ts
+++ src/controller/Login.controller.ts
@@ -7,4 +7,10 @@ export default class LoginController {
     const token = await LoginService.createUser(userInfo);
     return res.status(201).json({ token });
   }
+
+  public verifyLogin = async (req: Request, res: Response): Promise<Response> => {
+    const userInfo = req.body;
+    const token = await LoginService.verifyLogin(userInfo);
+    return res.status(200).json({ token });
+  }
 }
