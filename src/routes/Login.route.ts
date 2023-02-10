cc949ecdbc03213ea22c199b916ed8a6f3d6820a
Ricardo_Souza <rl03souza@gmail.com>
2023-02-10 15:19:44 -0300

    Feat: creating route post /login

--- src/routes/Login.route.ts
+++ src/routes/Login.route.ts
@@ -6,5 +6,6 @@ const loginRouter = Router();
 const loginController = new LoginController();
 
 loginRouter.post('/register', loginController.registerUser);
+loginRouter.post('/', loginController.verifyLogin);
 
 export default loginRouter;
