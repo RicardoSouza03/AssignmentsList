107eadf731deae8e8481e603ce2004ad3d0247f3
Ricardo_Souza <rl03souza@gmail.com>
2023-02-10 18:50:22 -0300

    Feat: creatign method to prevent register with same email

--- src/service/Login.service.ts
+++ src/service/Login.service.ts
@@ -3,15 +3,27 @@ import User from '../database/models/User';
 import { checkPassword, encryptPassword } from '../utils/bcrypt';
 import { generateToken } from '../utils/jwtToken';
 import LoginValidator from '../utils/validations/Login.validations';
+import sequelize = require('sequelize');
 
 export default class LoginService {
   private static loginValidations = new LoginValidator();
 
+  private static async userByEmail(userEmail: string): Promise<IUser | null> {
+    const foundUser = await User.findOne({ where: { email: userEmail.toLowerCase() }});
+    return foundUser;
+  }
+
   public static async createUser(user: Omit<IUser, 'id'>): Promise<string> {
     this.loginValidations.checkUserInfo(user);
-    
+    const userAlreadyExists = await this.userByEmail(user.email);
+    this.loginValidations.userEmailMatch(userAlreadyExists, false);
+
     const hash = encryptPassword(user.password);
-    const { dataValues: createdUser } = await User.create({ ...user, password: hash });
+    const { dataValues: createdUser } = await User.create({ 
+      ...user,
+      email: user.email.toLowerCase(),
+      password: hash
+    });
     
     this.loginValidations.checkUserCreation(createdUser);
 
@@ -23,8 +35,8 @@ export default class LoginService {
     this.loginValidations.checkUserEmail(loginInfo.email);
     this.loginValidations.checkUserPassword(loginInfo.password);
 
-    const foundUser = await User.findOne({ where: { email: loginInfo.email }});
-    this.loginValidations.checkUserExistence(foundUser);
+    const foundUser = await this.userByEmail(loginInfo.email);
+    this.loginValidations.userEmailMatch(foundUser, true);
     
     const passwordCheck = checkPassword(loginInfo.password, foundUser?.password as string);
     this.loginValidations.userPasswordMatch(passwordCheck);
