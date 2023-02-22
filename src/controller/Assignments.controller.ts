708edcd2171d38d8a778946a3cf8912b608b8b7e
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 16:02:01 -0300

    Refactor: includes token

--- src/controller/Assignments.controller.ts
+++ src/controller/Assignments.controller.ts
@@ -3,8 +3,10 @@ import { Request, Response } from 'express';
 
 export default class AssignmentController {
   public createAssignment = async (req: Request, res: Response): Promise<Response> => {
+    const token = req.headers.authorization;
     const newAssignment = req.body;
-    const createdAssignment = await AssignmentsService.createAssignment(newAssignment);
+
+    const createdAssignment = await AssignmentsService.createAssignment(newAssignment, token);
     return res.status(201).json({ createdAssignment });
   }
 }
