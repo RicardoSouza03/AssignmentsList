614bb8f602a2eab7c572ec091f00f00997a6e5a1
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 19:21:14 -0300

    Fix: removing password from update method return and add validation to name

--- src/service/Profile.service.ts
+++ src/service/Profile.service.ts
@@ -2,8 +2,11 @@ import { JwtPayload } from 'jsonwebtoken';
 import IUser from '../interfaces/IUser';
 import User from '../database/models/User';
 import { verifyToken } from '../utils/jwtToken';
+import LoginValidator from '../utils/validations/Login.validations';
 
 export default class ProfileService {
+  private static validate = new LoginValidator();
+
   public static getUserInfo(token: string | undefined): Omit<IUser, 'password'> {
     const userPayload = verifyToken(token as string);
     const { data } = userPayload as JwtPayload;
@@ -12,10 +15,11 @@ export default class ProfileService {
 
   public static async updateUserName(newName: string, token: string | undefined): Promise<IUser> {
     const { id, email } = this.getUserInfo(token);
+    this.validate.checkUserName(newName);
 
     await User.update({ name: newName }, { where: { id, email } });
 
-    const updatedUser = await User.findByPk(id);
+    const updatedUser = await User.findByPk(id, { attributes: { exclude: ['password'] } });
     return updatedUser as IUser;
   }
 }
