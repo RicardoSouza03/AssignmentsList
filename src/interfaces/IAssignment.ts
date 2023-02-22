01db7987f31b5cc6db8e69dd061eb38e77e6bf73
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 16:29:48 -0300

    Refactor: interfaces of assignments

--- src/interfaces/IAssignment.ts
+++ src/interfaces/IAssignment.ts
@@ -2,9 +2,6 @@ export default interface IAssignment {
   id?: number;
   userId: number;
   description: string;
-}
-
-export interface IAssignmentCreated extends IAssignment {
   created: Date;
   updated: Date;
 }
