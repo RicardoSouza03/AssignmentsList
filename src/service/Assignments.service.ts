107c0a9eaa50bd61cc92611ad5bc18438e8b1266
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 17:15:54 -0300

    Feat: adding validations to assignment service

--- src/service/Assignments.service.ts
+++ src/service/Assignments.service.ts
@@ -2,13 +2,17 @@ import { JwtPayload } from 'jsonwebtoken';
 import Assignment from '../database/models/Assignment';
 import IAssignment from '../interfaces/IAssignment';
 import { verifyToken } from '../utils/jwtToken';
+import AssignmentValidator from '../utils/validations/Assignment.validations';
 
 export default class AssignmentsService {
+  private static validations = new AssignmentValidator();
+
   public static async createAssignment(
     assignment: Omit<IAssignment, 'id'>,
     token: string | undefined
   ): Promise<IAssignment>
   {
+    this.validations.validateDescription(assignment.description); 
     const userPayload = verifyToken(token as string);
     const { data: { id } } = userPayload as JwtPayload;
 
