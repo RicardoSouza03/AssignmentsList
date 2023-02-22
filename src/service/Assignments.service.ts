c1567b6b22f3571eb740a7328f2a201a046b466f
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 17:25:45 -0300

    Feat: creating new method to get all user assignments

--- src/service/Assignments.service.ts
+++ src/service/Assignments.service.ts
@@ -7,16 +7,28 @@ import AssignmentValidator from '../utils/validations/Assignment.validations';
 export default class AssignmentsService {
   private static validations = new AssignmentValidator();
 
+  private static getIdFromToken(token: string | undefined): number {
+    const userPayload = verifyToken(token as string);
+    const { data: { id } } = userPayload as JwtPayload;
+    return id;
+  }
+
   public static async createAssignment(
     assignment: Omit<IAssignment, 'id'>,
     token: string | undefined
   ): Promise<IAssignment>
   {
     this.validations.validateDescription(assignment.description); 
-    const userPayload = verifyToken(token as string);
-    const { data: { id } } = userPayload as JwtPayload;
+    const id = this.getIdFromToken(token);
 
     const createdAssignment = await Assignment.create({ ...assignment, userId: id });
     return createdAssignment;
+  }
+
+  public static async getUserAssignments(token: string | undefined): Promise<IAssignment[]> {
+    const id = this.getIdFromToken(token);
+    const userAssignments = await Assignment.findAll({ where: { userId: id }});
+
+    return userAssignments;
   }
 }
