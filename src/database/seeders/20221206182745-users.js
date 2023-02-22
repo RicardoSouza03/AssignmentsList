1c054b74c727ce6f5b9976dcdd30225447d854de
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 19:33:33 -0300

    Refactor: user seeders passwords, encrypt all passwords

--- src/database/seeders/20221206182745-users.js
+++ src/database/seeders/20221206182745-users.js
@@ -8,19 +8,22 @@ module.exports = {
       id: 1,
       name: 'John Doe',
       email: 'jhonDoe@gmail.com',
-      password: 'jhonDoePassword'
+      password: '$2a$10$aj0ImWkHWHX058hdJbX7r.YuxQ/eFqCsmnhngFk9A//P2SBjybQha'
+      // senha real: jhonDoePassword
     },
     {
       id: 2,
       name: 'Richard Jonson',
       email: 'richardJonson@gmail.com',
-      password: 'richardJonsonPassword'
+      password: '$2a$10$tTb.5oL/haEuc9ZyDBgDROp2CIasOVDw9diuPdq2UE.FM9i1n7wUK'
+      // senha real: richardJonsonPassword
     },
     {
       id: 3,
       name: 'Rachel Katerine',
       email: 'rachelKaterine@gmail.com',
-      password: 'rachelKaterinePassword'
+      password: '$2a$10$t1WZjAH09jymAHG/aLf4NeA/ApytOMTlfL1tLynz.o4mLLrnatglq'
+      // senha real: rachelKaterinePassword
     },
     ], { timestamps: false });
   },
