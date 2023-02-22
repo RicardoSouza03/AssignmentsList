7a2cd86fd2aa9a2dc127d62601d69b4f194a6240
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 19:05:28 -0300

    Refactor: method to get user info, to get it through token

--- src/service/Profile.service.ts
+++ src/service/Profile.service.ts
@@ -1,24 +1,10 @@
 import { JwtPayload } from 'jsonwebtoken';
-import User from '../database/models/User';
-import IUser from '../interfaces/IUser';
 import { verifyToken } from '../utils/jwtToken';
 
 export default class ProfileService {
-  private static getIdFromToken(token: string | undefined): number {
+  public static getUserInfo(token: string | undefined): number {
     const userPayload = verifyToken(token as string);
-    const { data: { id } } = userPayload as JwtPayload;
-    return id;
+    const { data } = userPayload as JwtPayload;
+    return data;
   }
-
-  public static async getUserInfo(email: string, token: string | undefined): Promise<IUser | null> {
-    const id = this.getIdFromToken(token);
-
-    const foundUser = await User.findOne({ 
-      where: { email: email.toLowerCase(), id },
-      attributes: { exclude: ['password'] }
-    });
-    return foundUser;
-  }
-
-
 }
