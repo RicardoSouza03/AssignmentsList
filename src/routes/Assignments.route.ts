4c83d4f3445cb70c239438a63a8b80e169fee865
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 18:21:17 -0300

    Feat: creating route delete to assignments

--- src/routes/Assignments.route.ts
+++ src/routes/Assignments.route.ts
@@ -8,5 +8,6 @@ const assignmentController = new AssignmentController();
 assignmentRoute.post('/', assignmentController.createAssignment);
 assignmentRoute.get('/', assignmentController.getUserAssignments);
 assignmentRoute.put('/:id', assignmentController.updateAssignment);
+assignmentRoute.delete('/:id', assignmentController.deleteAssignment);
 
 export default assignmentRoute;
