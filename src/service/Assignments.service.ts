5a0d30c767b5c378b01fb8c9be9000fc89503bbd
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 15:42:21 -0300

    Feat: creating method to create an assignment

--- src/service/Assignments.service.ts
+++ src/service/Assignments.service.ts
@@ -1,3 +1,9 @@
+import Assignment from '../database/models/Assignment';
+import IAssignment, { IAssignmentCreated } from '../interfaces/IAssignment';
+
 export default class AssignmentsService {
-  
+  public static async createAssignment(assignment: Omit<IAssignment, 'id'> ): Promise<IAssignmentCreated> {
+    const createdAssignment = await Assignment.create(assignment);
+    return createdAssignment;
+  }
 }
