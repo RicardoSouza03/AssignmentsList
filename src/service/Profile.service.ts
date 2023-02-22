3c60453e5626118113d9abaff227e71594c318f8
Ricardo_Souza <rl03souza@gmail.com>
2023-02-22 19:11:24 -0300

    Feat: creating method to update user name

--- src/service/Profile.service.ts
+++ src/service/Profile.service.ts
@@ -1,10 +1,21 @@
 import { JwtPayload } from 'jsonwebtoken';
+import IUser from '../interfaces/IUser';
+import User from '../database/models/User';
 import { verifyToken } from '../utils/jwtToken';
 
 export default class ProfileService {
-  public static getUserInfo(token: string | undefined): number {
+  public static getUserInfo(token: string | undefined): Omit<IUser, 'password'> {
     const userPayload = verifyToken(token as string);
     const { data } = userPayload as JwtPayload;
     return data;
+  }
+
+  public static async updateUserName(newName: string, token: string | undefined): Promise<IUser> {
+    const { id, email } = this.getUserInfo(token);
+
+    await User.update({ name: newName }, { where: { id, email } });
+
+    const updatedUser = await User.findByPk(id);
+    return updatedUser as IUser;
   }
 }
